e24ef85916c50874149de8b98023656343c24851
Server <elderstarsgiron@gmail.com>
2013-11-17 13:32:51 +0100

    initial commit

---
+++ automatic_commits.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+#  automatic_commits.sh
+#  iGitpad
+#
+#  Created by Johannes Lund on 2013-09-14.
+#  Copyright (c) 2013 Anviking. All rights reserved.
+
+while [ 1 ];
+do
+NOW=$(date +"%H:%M %d/%m-%Y")
+git commit -am "Backup $NOW";
+sleep 3600;
+done
